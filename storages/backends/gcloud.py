7c33b8a5d419d28d089a9594086e21aca8ad775c
Harry Karvonen <harry.karvonen@onebyte.fi>
2022-07-15 16:06:36 +0300

    gcloud: Use DEFAULT_RETRY when uploading a file.

--- storages/backends/gcloud.py
+++ storages/backends/gcloud.py
@@ -19,6 +19,7 @@ try:
     from google.cloud.exceptions import NotFound
     from google.cloud.storage import Blob, Client
     from google.cloud.storage.blob import _quote
+    from google.cloud.storage.retry import DEFAULT_RETRY
 except ImportError:
     raise ImproperlyConfigured("Could not load Google Cloud Storage bindings.\n"
                                "See https://github.com/GoogleCloudPlatform/gcloud-python")
@@ -84,6 +85,7 @@ class GoogleCloudFile(File):
                 blob_params = self._storage.get_object_parameters(self.name)
                 self.blob.upload_from_file(
                     self.file, rewind=True, content_type=self.mime_type,
+                    retry=DEFAULT_RETRY,
                     predefined_acl=blob_params.get('acl', self._storage.default_acl))
             self._file.close()
             self._file = None
@@ -171,7 +173,7 @@ class GoogleCloudStorage(BaseStorage):
         for prop, val in blob_params.items():
             setattr(file_object.blob, prop, val)
 
-        file_object.blob.upload_from_file(content, rewind=True, size=content.size, **upload_params)
+        file_object.blob.upload_from_file(content, rewind=True, retry=DEFAULT_RETRY, size=content.size, **upload_params)
         return cleaned_name
 
     def get_object_parameters(self, name):
